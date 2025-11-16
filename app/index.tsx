7fe91903475517e9c125b29e9be32f17476278b1
Ayush Wankhede <ayushwankhede9@gmail.com>
2025-11-16 23:20:29 +0530

    This is furrever expo project

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+"use dom";
+import { Text, View } from "react-native";
+import React from "react";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <>
+      <View className="  ">
+        <Text className=" text-red-600 font-bold text-2xl">Welcome to furrever</Text>
+      </View>
+      
+    </>
+
+  );
+}
